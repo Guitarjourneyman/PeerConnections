0a1ce819d9f73f978d82d5cf68b59a0cffa07c24
Guitarjourneyman <149582970+Guitarjourneyman@users.noreply.github.com>
2024-10-04 11:38:29 +0900

    Update Server.swift
    Added IP log in Send

--- MultiConnect/Server.swift
+++ MultiConnect/Server.swift
@@ -3,7 +3,7 @@
 //  MultiConnect
 //
 //  Created by michal on 29/11/2020.
-//
+//  Modified by aye on 04/10/2024
 
 import Foundation
 import Network
@@ -14,7 +14,6 @@ let server = try? Server()
 class Server {
 
     let listener: NWListener
-
     var connections: [Connection] = []
 
     init() throws {
@@ -42,8 +41,45 @@ class Server {
     }
 
     func send() {
+        guard let ipAddress = getLocalIPAddress() else {
+            log("Failed to get IP address")
+            return
+        }
+        
         connections.forEach {
-            $0.send("super message from the server! \(Int(Date().timeIntervalSince1970))")
+            let message = "super message from the server! \(Int(Date().timeIntervalSince1970)) | IP: \(ipAddress)"
+            $0.send(message)
         }
     }
+    
+    // Function to get the local IP address
+    func getLocalIPAddress() -> String? {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+                
+                let interface = ptr?.pointee
+                let addrFamily = interface?.ifa_addr.pointee.sa_family
+                
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6),
+                   let ifaName = interface?.ifa_name,
+                   String(cString: ifaName) == "en0" { // en0 is typically the Wi-Fi interface
+                    var addr = interface?.ifa_addr.pointee
+                    var hostName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(&addr!, socklen_t(interface!.ifa_addr.pointee.sa_len),
+                                &hostName, socklen_t(hostName.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostName)
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        
+        return address
+    }
 }
+
